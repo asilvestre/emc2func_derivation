809b267deb5c52f200619069ceadc345113c4e12
asilvestre <antoni.silvestre@gmail.com>
2013-03-06 00:45:14 +0100

    simplifying code

--- derive.hs
+++ derive.hs
@@ -1,7 +1,10 @@
 --parsing input
 data UOperator = Sqrt | Sin | Cos | Ln | Exp deriving(Enum, Eq)
 data BOperator = Sum | Diff | Div | Mul | Pow deriving(Enum, Eq)
+infix 9 `C` 
+infix 9 `V` 
 data Expression = UnExp UOperator Expression | BiExp BOperator Expression Expression | C Int | V Char
+
 
 instance Eq Expression where
 	C x == C y = y == x
@@ -24,21 +27,31 @@ instance Show Expression where
 	show (UnExp Ln e1) = "ln(" ++ show e1 ++ ")"
 	show (UnExp Exp e1) = "e^" ++ show e1 ++ ""
 	show (BiExp Sum e1 e2) = "(" ++ show e1 ++ " + " ++ show e2 ++ ")"
-	show (BiExp Diff e1 e2) = show e1 ++ " - " ++ show e2
+	show (BiExp Diff e1 e2) = "(" ++ show e1 ++ " - " ++ show e2 ++ ")"
 	show (BiExp Div e1 e2) = "(" ++ show e1 ++ " / " ++ show e2 ++ ")"
 	show (BiExp Mul e1 e2) = "(" ++ show e1 ++ " * " ++ show e2 ++ ")"
 	show (BiExp Pow e1 e2) = "" ++ show e1 ++ "^" ++ show e2 ++ ""
 
 
+infixl 6 +:
 (+:) a b = BiExp Sum a b
+infixl 6 -:
 (-:) a b = BiExp Diff a b
+infixl 7 *:
 (*:) a b = BiExp Mul a b
+infixl 7 /:
 (/:) a b = BiExp Div a b
+infixl 8 ^:
 (^:) a b = BiExp Pow a b
+infixl 9 `sqrt'`
 sqrt' a = UnExp Sqrt a
+infixl 9 `sin'`
 sin' a = UnExp Sin a
+infixl 9 `cos'`
 cos' a = UnExp Cos a
+infixl 9 `ln'`
 ln' a = UnExp Ln a
+infixl 9 `e'`
 e' a = UnExp Exp a
 
 
@@ -51,17 +64,16 @@ realfunc Pow = (^)
 
 
 diff :: Expression -> Char -> Expression
-diff (UnExp Sqrt e1) x = ((C 1) /: ((C 2) *: (e1 ^: (C 2)))) *: (diff e1 x)
-diff (UnExp Sin e1) x = (cos' e1) *: (diff e1 x)
-diff (UnExp Cos e1) x = ((C (-1)) *: (sin' e1)) *: (diff e1 x)
-diff (UnExp Ln e1) x = ((C 1) /: e1) *: (diff e1 x)
-diff (UnExp Exp e1) x = (e' e1) *: (diff e1 x)
+diff (UnExp Sqrt e1) x = (C 1 /: (C 2 *: e1 ^: C 2)) *: (diff e1 x)
+diff (UnExp Sin e1) x = cos' e1 *: (diff e1 x)
+diff (UnExp Cos e1) x = C (-1) *: sin' e1 *: (diff e1 x)
+diff (UnExp Ln e1) x = (C 1 /: e1) *: (diff e1 x)
+diff (UnExp Exp e1) x = e' e1 *: (diff e1 x)
 diff (BiExp Pow e1 (C 0)) _ = C 0
-diff (BiExp Pow e1 (C c)) x = let c' = C c in c' *: (e1 ^: (c' -: (C 1)))
-diff (BiExp Pow e1 (V v)) x = let v' = V v in if v /= x then v' *: (e1 ^: (v' -: (C 1))) else diff (BiExp Pow e1 v') x
-diff (BiExp Pow e1 e2) x = e1 *: (((diff e2 x) *: (ln' e1)) +: ((e2 /: e1) *: (diff e1 x)))
-diff (BiExp Mul e1 e2) x = ((diff e1 x) *: e2) +: (e1 *: (diff e2 x))
-diff (BiExp Div e1 e2) x = (((diff e1 x) *: e2) -: (e1 *: (diff e2 x))) /: (e2 ^: (C 2))
+diff (BiExp Pow e1 (C c)) x = let e1' = diff e1 x in C c *: e1 ^: (C c -: C 1) *: e1'
+diff (BiExp Pow e1 e2) x = let e1' = diff e1 x; e2' = diff e2 x in e1 *: (e2' *: ln' e1 +: ((e2 /: e1) *: e1'))
+diff (BiExp Mul e1 e2) x = let e1' = diff e1 x; e2' = diff e2 x in e1' *: e2 +: e1 *: e2'
+diff (BiExp Div e1 e2) x = let e1' = diff e1 x; e2' = diff e2 x in (e1' *: e2 -: e1 *: e2') /: (e2 ^: C 2)
 diff (BiExp Diff e1 e2) x = (diff e1 x) -: (diff e2 x)
 diff (BiExp Sum e1 e2) x = (diff e1 x) +: (diff e2 x)
 diff (V x') x = if x' == x then C 1 else C 0
@@ -80,7 +92,7 @@ simplify :: Expression -> Expression
 simplify (BiExp op (C x) (C x')) = C (realfunc op x x')
 simplify (BiExp Sum e (C 0)) = simplify e
 simplify (BiExp Sum (C 0) e) = simplify e
-simplify (BiExp Sum e e') = if e == e' then (C 2) *: simplify(e) else simplify(e) +: simplify(e')
+simplify (BiExp Sum e e') = if e == e' then C 2 *: simplify(e) else simplify(e) +: simplify(e')
 simplify (BiExp Diff e (C 0)) = simplify(e)
 simplify (BiExp Diff (C 0) e) = simplify(e)
 simplify (BiExp Diff e e') = if e == e' then C 0 else simplify(e) -: simplify(e')
@@ -88,14 +100,18 @@ simplify (BiExp Mul (C 0) e) = C 0
 simplify (BiExp Mul e (C 0)) = C 0 
 simplify (BiExp Mul (C 1) e) = simplify(e)
 simplify (BiExp Mul e (C 1)) = simplify(e)
-simplify (BiExp Mul e e') = if e == e' then simplify(e) ^: (C 2) else simplify(e) *: simplify(e')
+simplify (BiExp Mul (C x) (BiExp Mul (C y) e)) = simplify(C x *: C y) *: simplify e
+simplify (BiExp Mul (C x) (BiExp Mul e (C y))) = simplify(C x *: C y) *: simplify e
+simplify (BiExp Mul (V x) (V y)) = if x == y then V x ^: C 2 else V x *: V y
+simplify (BiExp Mul e e') = if e == e' then simplify(e) ^: C 2 else simplify(e) *: simplify(e')
 simplify (BiExp Div (C 0) e) = C 0 
 simplify (BiExp Div e (C 1)) = simplify e
 simplify (BiExp Div e e') = if e == e' then C 1 else simplify(e) -: simplify(e')
 simplify (BiExp Pow e (C 1)) = simplify e
 simplify (BiExp Pow e (C 0)) = C 1
+simplify (BiExp Pow (C 0) _) = C 0
 simplify (BiExp op e e') = BiExp op (simplify e) (simplify e')
-simplify (UnExp op e ) = UnExp op (simplify e)
+simplify (UnExp op e) = UnExp op (simplify e)
 simplify e = e
 
 simplifier e = let e' = simplify e in if exact e e' then e else simplifier e'
